3df572037268dcb2e878a52203e693e4e13413da
Marwin Steiner <marwin.steiner@gmail.com>
2024-10-26 22:44:29 +0100

    updated .gitignore and config files

--- config.py
+++ config.py
@@ -3,7 +3,7 @@ from dynaconf import Dynaconf
 
 settings = Dynaconf(
     envvar_prefix="DYNACONF",
-    settings_files=['settings.toml', '.secrets.toml'],
+    settings_files=['settings.json', '.secrets.json'],
 )
 
 # `envvar_prefix` = export envvars with `export DYNACONF_FOO=bar`.
